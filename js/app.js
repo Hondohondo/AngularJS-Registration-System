b281186f325e11a07d5519719e17cb4054bc2e4b
hondohondo <mnandi19@jcu.edu>
2020-04-03 13:31:33 -0400

    Registration System AngularJs

---
+++ js/app.js
@@ -0,0 +1,25 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.
+      when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'RegistrationController'
+    }).
+      when('/register', {
+      templateUrl: 'views/register.html',
+      controller: 'RegistrationController'
+    }).
+      when('/success', {
+      templateUrl: 'views/success.html',
+      controller: 'SuccessController'
+    }).
+      otherwise({
+      redirectTo: '/login'
+    });
+}]);
+
+//
+// myApp.controller('appController', ['$scope', function ($scope) {
+//   $scope.message = "Welcome to my App";
+// }])
---
+++ js/controllers/registration.js
@@ -0,0 +1,32 @@
+
+myApp.controller('RegistrationController', ['$scope', function ($scope) {
+    // $scope.message = "Welcome to my App";
+
+  $scope.login = function () {
+      $scope.message = "Welcome" + $scope.user.email;
+  };
+
+  $scope.register = function () {
+      $scope.message = "Welcome" + $scope.user.firstname;
+  };
+
+}]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// myApp.controller('appController', ['$scope', function ($scope) {
+//   $scope.message = "Welcome to my App";
+// }])
